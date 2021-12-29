569ece19180e8660adbeac200728016843b69872
Hiroomi Okoshi <hiroomi@okoshi.net>
2021-12-30 02:10:28 +0900

    Add code for parameter.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log("Hello World!")
+import { parse } from "comment-json"
+import * as fs from "fs"
+
+const params = parse(fs.readFileSync("./package.json", "utf8").toString()).gncc as Gncc.Params
+console.log(JSON.stringify(params))
