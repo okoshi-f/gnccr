38a5b0a33b602d51d1ab2c525ea6ed6faf3c3be5
Hiroomi Okoshi <hiroomi@okoshi.net>
2021-12-31 02:43:14 +0900

    Modify types for destination

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,7 +1,9 @@
-declare module Gncc {
+declare module Gnccr {
   export type Params = {
     owner: string
     repo: string
     keywords: Array<string>
+    since: string
+    destination: string
   }
 }
