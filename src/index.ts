5f4cb593e0855b9a52d4ab583e7dcc55941ed9f9
Hiroomi Okoshi <hiroomi@okoshi.net>
2022-01-02 03:56:18 +0900

    Modify entry point.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 #!/usr/bin/env node
 
-import { MainController } from "controllers"
+import * as fs from "fs"
+
+const productionPath = `${ process.cwd() }/node_modules/gnccr/dist/controllers`
+const developPath = "controllers"
+const path = fs.existsSync(productionPath) ? productionPath : developPath
+const { MainController } = require(path)
 
 MainController.execute()
+  // @ts-ignore
   .then((exitCode) => {
     process.exit(exitCode ?? 0)
   })
+  // @ts-ignore
   .catch((e) => {
     console.error(e)
     process.exit(1)
