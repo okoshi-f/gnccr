3bcada0030ca7da19c8a831d13b191d142f138f5
Hiroomi Okoshi <hiroomi@okoshi.net>
2022-01-03 00:06:30 +0900

    Fix executable file.

---
+++ bin/index.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+
+require(`${process.cwd()}/node_modules/gnccr/dist`)
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,11 @@
 #!/usr/bin/env node
 
-import * as fs from "fs"
-
-const productionPath = `${ process.cwd() }/node_modules/gnccr/dist/controllers`
-const developPath = "controllers"
-const path = fs.existsSync(productionPath) ? productionPath : developPath
-const { MainController } = require(path)
+import { MainController } from "controllers"
 
 MainController.execute()
-  // @ts-ignore
   .then((exitCode) => {
     process.exit(exitCode ?? 0)
   })
-  // @ts-ignore
   .catch((e) => {
     console.error(e)
     process.exit(1)
